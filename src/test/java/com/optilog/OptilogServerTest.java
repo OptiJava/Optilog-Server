03860c5d32a711e0cc904da2a8eb45af701026a7
OptiJava <OptiJava@protonmail.com>
2022-06-03 19:43:43 +0800

    2022.6-Server(fix bug in path)

--- src/test/java/com/optilog/OptilogServerTest.java
+++ src/test/java/com/optilog/OptilogServerTest.java
@@ -3,8 +3,9 @@ package com.optilog;
 import server.optilog.log.Log;
 
 public class OptilogServerTest {
-    public static void main(String[] args) {
-        Log log = Log.initLog("D:\\Program\\Feishu\\app\\assets\\object\\apps\\Intellij-IDEA\\Project\\Optilog-Server\\src\\test\\resources\\Settings.json");
-        log.command("%StartServer");
-    }
+	public static void main(String[] args) {
+		Log log = Log.initLog("D:\\Program\\Project\\resources\\app\\Git\\Projects\\Optilog-Server\\src\\test\\resources\\Settings.json");
+		//log.command("%StartServer");
+		log.debug("Server");
+	}
 }
