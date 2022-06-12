ded93935470a963840e3b1c4d7f4ee58ac3ea2e5
OptiJava <OptiJava@protonmail.com>
2022-06-12 15:12:32 +0800

    2022.6-Server(fix bug:LevelBuild Log will not be sent to server)

--- src/main/java/server/optilog/util/exception/CreateFileErrorException.java
+++ src/main/java/server/optilog/util/exception/CreateFileErrorException.java
@@ -1,18 +1,17 @@
 package server.optilog.util.exception;
 
-
 public final class CreateFileErrorException extends OptilogException {
     public CreateFileErrorException() {
     }
-    
+
     public CreateFileErrorException(String message, Throwable cause) {
         super(message, cause);
     }
-    
+
     public CreateFileErrorException(String message) {
         super(message);
     }
-    
+
     public CreateFileErrorException(Throwable cause) {
         super(cause);
     }
--- src/test/java/com/optilog/OptilogServerTest.java
+++ src/test/java/com/optilog/OptilogServerTest.java
@@ -5,7 +5,7 @@ import server.optilog.log.Log;
 public class OptilogServerTest {
 	public static void main(String[] args) {
 		Log log = Log.initLog("D:\\Program\\Project\\resources\\app\\Git\\Projects\\Optilog-Server\\src\\test\\resources\\Settings.json");
-		//log.command("%StartServer");
+		log.command("%StartServer");
 		log.debug("Server");
 	}
 }
